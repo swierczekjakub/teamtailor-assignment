dbafe7639933d43eade3e2c28ebc33347889fe4a
swierczekjakub <swierczekjakub1@gmail.com>
2024-12-17 19:39:28 +0100

    added rate limiter.

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,17 @@ import express from "express";
 import "express-async-errors";
 import * as dotenv from 'dotenv';
 dotenv.config();
+import { rateLimit } from 'express-rate-limit'
 import {downloadRouter} from "./routes/download";
 
 const app = express();
+const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    limit: 5,
+})
 
 app.use(express.static('public'));
+app.use(limiter);
 app.use('/download', downloadRouter);
 
 app.listen(3000, 'localhost', () => {
